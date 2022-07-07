3f99aa50acbdcc4fc8808bf623ea8c80bb4b69f0
yolocka <yolocka@bk.ru>
2022-07-07 15:54:49 +0300

    two tests added

--- app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
+++ app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
@@ -32,6 +32,16 @@ class EmailValidatorTest {
     }
 
     @Test
+    fun emailValidator_InvalidEmailNoDomain_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailDoubleAt_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@@email.com"))
+    }
+
+    @Test
     fun emailValidator_EmptyString_ReturnsFalse() {
         assertFalse(EmailValidator.isValidEmail(""))
     }
